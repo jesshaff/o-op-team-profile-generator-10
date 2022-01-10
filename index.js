a3268e68bdba7a917bf21a7e6b63545bd9a937b2
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2022-01-09 21:24:25 -0500

    outline for criteria added to index.js and inquirer and jest installed

---
+++ index.js
@@ -0,0 +1,19 @@
+// Accept user input for the following:
+// Team members information 
+
+// Generate an HTML file based off user input
+
+//  When I click email address in HTML, email opens and populated TOL field with email address
+
+// When i click GitHub username, GitHub profile opens in a new tab
+
+// At application start, prompt to enter team managers name, employee ID, email address & office #
+
+// Enter team managers name, employee ID, email address & office # > presented with menu w/ option to add an engineer or intern, or to finish building team
+
+// Select engineer option > Prompted to enter engineers name, ID, email, and GitHub username > then taken back to menu
+
+// Select intern option > Prompted to enter interns name, ID, email, and school > then taken back to menu
+
+// Once finished building team > exit the application > HTML is generated 
+
